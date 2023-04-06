1e26b9003e11fb468a01a5f2dc7747b1eb04b137
adil tahiri <rajaadil1995@gmail.com>
2023-04-06 23:57:03 +0000

    add proity Queue in min heap

---
+++ queue/priorityQueue.js
@@ -0,0 +1,74 @@
+/*Description // priority Queue
+// Priority Queue class using Minimum Binary Heap
+
+*/
+
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+
+  enqueue(value, priority) {
+    this.heap.push({ value, priority });
+    this.#bubbleUp(this.heap.length - 1);
+  }
+  dequeue() {
+    const min = this.heap[0];
+    const end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.#bubbleDown(0);
+    }
+    return min;
+  }
+  #bubbleUp(index) {
+    const element = this.heap[index];
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.heap[parentIndex] = element;
+      this.heap[index] = parent;
+      index = parentIndex;
+    }
+  }
+  #bubbleDown(index) {
+    const length = this.heap.length;
+    const element = this.heap[index];
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.heap[leftChildIndex];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.heap[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.heap[index] = this.heap[swap];
+      this.heap[swap] = element;
+      index = swap;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+}
+
+export { PriorityQueue };
