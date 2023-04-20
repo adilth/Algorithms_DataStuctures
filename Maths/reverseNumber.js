0fee118a0fa75caea6aab54b4200915cccbc3c1b
adil tahiri <rajaadil1995@gmail.com>
2023-04-20 23:59:32 +0000

    reverse al number digit

---
+++ Maths/reverseNumber.js
@@ -0,0 +1,19 @@
+/*Description // reverse number
+ReverseNumber return the reversed value of the given number.
+
+*/
+function reverseNumber(num) {
+  if (typeof num !== "number") {
+    return new TypeError("Argument is not a number.");
+  }
+  let reverseNum = 0;
+  while (num > 0) {
+    const lastDigit = num % 10;
+    reverseNum = reverseNum * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+  return reverseNum;
+}
+
+console.log(reverseNumber(12)); //21
+console.log(reverseNumber(321)); //123
