493ce2fc6e3d74142c34d8f4721248ff43688206
adil tahiri <rajaadil1995@gmail.com>
2023-04-25 00:18:10 +0100

    find a perfect cube of number

--- Maths/perfectCube.js
+++ Maths/perfectCube.js
@@ -1,5 +1,7 @@
 /*Description // perfect cube
 This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors
+
+ the function perfectCube takes a number as an argument and returns true if the number is a perfect cube (i.e., the cube root of the number is an integer), and false otherwise.
 */
 
 const perfectCube = (num) =>
