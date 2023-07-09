8d668503fa3ae92d091b1e6ac2e052f391c514b6
adil tahiri <rajaadil1995@gmail.com>
2023-07-09 23:02:42 +0100

    git the bit of number

---
+++ Maths/getAndSetBit.js
@@ -0,0 +1,28 @@
+/*Description //get bit */
+/**
+ * @param {number} number
+ * @param {number} bitPosition - zero based.
+ * @return {number}
+ */
+
+function getBit(number, bitPosition) {
+  return (number >> bitPosition) & 1;
+}
+
+function setBit(number, bitPosition) {
+  return number | (1 << bitPosition);
+}
+
+console.log(getBit(10, 1)); //1
+console.log(getBit(10, 4)); //0
+console.log(getBit(64, 6)); //1
+console.log(getBit(1, 1)); //0
+console.log(getBit(15, 1)); //1
+console.log(getBit(100, 0)); //0
+console.log(setBit(10, 1)); //10
+console.log(setBit(10, 4)); //26
+console.log(setBit(64, 6)); //64
+console.log(setBit(1, 1)); //3
+console.log(setBit(15, 1)); //15
+console.log(setBit(100, 0)); //101
+log;
