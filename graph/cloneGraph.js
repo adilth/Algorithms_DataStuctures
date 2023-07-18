8f8602da671463e46d4e4cbe102d46420a24561d
adil tahiri <rajaadil1995@gmail.com>
2023-07-18 02:35:31 +0100

    clone a graph

--- graph/cloneGraph.js
+++ graph/cloneGraph.js
@@ -1,4 +1,4 @@
-/*Description // clone a graph
+/**Description // clone a graph
 Given a reference of a node in a connected undirected graph.
 
 Return a deep copy (clone) of the graph.
@@ -12,12 +12,19 @@ An adjacency list is a collection of unordered lists used to represent a finite 
 
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 */
-function Node(val, neighbors) {
-  this.val = val === undefined ? 0 : val;
-  this.neighbors = neighbors === undefined ? [] : neighbors;
+export default class Node {
+  constructor(val, neighbors) {
+    this.val = val === undefined ? 0 : val;
+    this.neighbors = neighbors === undefined ? [] : neighbors;
+  }
 }
-
-function cloneGraph(node) {
+/**
+ *
+ * @param {{val:Number, neighbors : []}} node
+ * @returns {{val:Number, neighbors : []}}
+ */
+export function cloneGraph(node) {
+  if (!(node instanceof Node)) return null;
   let fromOldToNew = {};
   const dfs = (node) => {
     if (!node) return node;
@@ -43,12 +50,3 @@ var cloneGraph = function (node, map = new Map()) {
   return n;
 };
 */
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-node1.neighbors = [node2, node4];
-node2.neighbors = [node1, node3];
-node3.neighbors = [node2, node4];
-node4.neighbors = [node1, node3];
-console.log(cloneGraph(node1));
