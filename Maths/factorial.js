36443f84df015615b1b15888c9a4f1a9815c3a55
adil tahiri <rajaadil1995@gmail.com>
2023-07-18 02:22:53 +0100

    implement the factorail

--- Maths/factorial.js
+++ Maths/factorial.js
@@ -1,5 +1,7 @@
-/*Description // factorial
+/**Description // factorial
 factorial, in mathematics, the product of all positive integers less than or equal to a given positive integer and denoted by that integer and an exclamation point. Thus, factorial seven is written 7!, meaning 1 × 2 × 3 × 4 × 5 × 6 × 7. Factorial zero is defined as equal to 1.
+@param {number} n
+@return {number}
 */
 
 export const factorial = (n) => {
