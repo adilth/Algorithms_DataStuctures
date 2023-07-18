708ad5dd1a67d8990ec178fc8a714245eab1aab3
adil tahiri <rajaadil1995@gmail.com>
2023-07-18 02:20:11 +0100

    find the kashnamurthy number

---
+++ Maths/KishnamurthyNumber.js
@@ -0,0 +1,22 @@
+/**Description //Kishnamurthy Number
+ * krishnamurthy number is a number the sum of the factorial of the all dights is equal to the number itself.
+ * @param {Number} number a number for checking is krishnamurthy number or not.
+ * @returns return correspond boolean value, if the number is krishnamurthy number return `true` else return `false`.
+ */
+
+import { factorial } from "./factorial.js";
+export default function KishnamurthyNumber(num) {
+  if (typeof num !== "number") {
+    return new TypeError("Argument is not a number.");
+  }
+
+  let sum = 0;
+  let newNum = num;
+
+  while (newNum > 0) {
+    const lastDigit = newNum % 10;
+    sum += factorial(lastDigit);
+    newNum = Math.floor(newNum / 10);
+  }
+  return sum === num;
+}
