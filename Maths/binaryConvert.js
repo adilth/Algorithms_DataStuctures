f49aed184dd40b4a9b6c68584dcee8b4fcbd3630
adil tahiri <rajaadil1995@gmail.com>
2023-07-18 02:18:29 +0100

    export the funtion for testing

--- Maths/binaryConvert.js
+++ Maths/binaryConvert.js
@@ -1,9 +1,12 @@
-/*Description // binary convert 
+/**Description // binary convert 
 convert number to binary format
-
+@param {number} number
+@returns number
 */
 
-function binaryConvert(num) {
+export default function binaryConvert(num) {
+  let isNegative = num < 0;
+  num = Math.abs(num);
   let p = 1;
   let binary = 0;
   while (num) {
@@ -12,10 +15,11 @@ function binaryConvert(num) {
     binary = rem * p + binary;
     p *= 10;
   }
-  return binary;
+
+  return isNegative ? -binary : binary;
 }
 
-const binaryConvertStringWay = (n) =>
-  n >= 0 ? n.toString(2) : (~n).toString(2);
-console.log(binaryConvert(12)); //1100
-console.log(binaryConvert(64)); //1000000
+export const binaryConvertStringWay = (n) =>
+  n >= 0 ? n.toString(2) : `-${Math.abs(n).toString(2)}`;
+
+console.log(binaryConvertStringWay(-9007199291));
