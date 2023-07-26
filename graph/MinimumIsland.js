88fe53844ace34f89ef38d4d22d1cbbc72ea3ded
adil <rajaadil1995@gmail.com>
2023-07-26 19:11:34 +0100

    add some fix for testing and export the function

--- graph/MinimumIsland.js
+++ graph/MinimumIsland.js
@@ -1,11 +1,13 @@
-/*Description //minimum island
+/**Description //minimum island
 
 Write a function, countTheMinIsland, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the size of the smallest island. An island is a vertically or horizontally connected region of land.
 
 You may assume that the grid contains at least one island.
+* @param {String[][]} grid
+* @returns {Number}
 */
-
-function countTheMinIsland(grid) {
+export default function countTheMinIsland(grid) {
+  if (!Array.isArray(grid) || !grid.length) return 0;
   let visited = new Set();
   let minCount = Infinity;
   for (let row = 0; row < grid.length; row += 1) {
@@ -16,10 +18,18 @@ function countTheMinIsland(grid) {
       }
     }
   }
-  return minCount;
+  return minCount === Infinity ? 0 : minCount;
 }
 
-const exploreGrid = (grid, row, col, visited) => {
+/**
+ *
+ * @param {String[][]} grid
+ * @param {String} row
+ * @param {String} col
+ * @param {Set<String>} visited
+ * @returns {number}
+ */
+export const exploreGrid = (grid, row, col, visited) => {
   let count = 1;
   let rowInbounds = row >= 0 && row < grid.length;
   let colInbounds = col >= 0 && col < grid[0].length;
