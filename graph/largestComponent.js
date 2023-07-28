e390ac880a751ab3c59d5bee36aae34d1e413ada
adil <rajaadil1995@gmail.com>
2023-07-28 20:37:43 +0100

    testing largest component

--- graph/largestComponent.js
+++ graph/largestComponent.js
@@ -3,7 +3,8 @@
 @return {Number}
 */
 
-const largestComponent = (graph) => {
+export default function largestComponent(graph) {
+  if (typeof graph !== "object" || !Object.keys(graph).length) return 0;
   let largest = 0;
   let visited = new Set();
   for (let node in graph) {
@@ -11,35 +12,23 @@ const largestComponent = (graph) => {
     if (size > largest) largest = size;
   }
   return largest;
-};
-
-function checkAllNodes(graph, currentNode, visited) {
-  let count = 1;
-  if (visited.has(currentNode)) return false;
-  visited.add(currentNode);
-  for (let neighbor of graph[currentNode]) {
-    count += checkAllNodes(graph, neighbor, visited);
-  }
-  return count;
 }
 
-let graph1 = largestComponent({
-  0: ["8", "1", "5"],
-  1: ["0"],
-  5: ["0", "8"],
-  8: ["0", "5"],
-  2: ["3", "4"],
-  3: ["2", "4"],
-  4: ["3", "2"],
-});
-console.log("🚀 ~ file: challengeGraphAlgolargestCompo.js:36 ~ graph1", graph1); //4
+/**
+ *
+ * @param {{Object.<number, string[]>}} graph
+ * @param {String} currentNode
+ * @param {Set<String>} visited
+ * @returns {Number }
+ */
+export function checkAllNodes(graph, currentNode, visited) {
+  let count = 1;
+  if (visited.has(currentNode)) return 0;
+  visited.add(currentNode);
+  for (let neighbor of graph[currentNode]) {
+    neighbor = neighbor.toString();
+    count += checkAllNodes(graph, neighbor, visited);
+  }
 
-let graph2 = largestComponent({
-  1: ["2"],
-  2: ["1", "8"],
-  6: ["7"],
-  9: ["8"],
-  7: ["6", "8"],
-  8: ["9", "7", "2"],
-});
-console.log("🚀 ~ file: challengeGraphAlgolargestCompo.js:46 ~ graph2", graph2); //6
+  return count;
+}
