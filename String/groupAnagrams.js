b19e991b5a30ce1581f1466cf04048cf74c0629d
adil <rajaadil1995@gmail.com>
2023-08-06 20:10:54 +0100

    export for testing and add fix if array contains not sting

--- String/groupAnagrams.js
+++ String/groupAnagrams.js
@@ -4,6 +4,23 @@
  */
 
 export default function groupAnagrams(strs) {
+  if (!Array.isArray(strs) || strs.length === 0) return [];
+  let map = new Map();
+  strs = strs.filter((str) => typeof str === "string");
+  let order = strs.map((str) => String(str).split("").sort().join(""));
+  for (let i = 0; i < strs.length; i++) {
+    const char = strs[i];
+    if (!map.has(order[i])) {
+      map.set(order[i], [char]);
+    } else {
+      map.get(order[i]).push(char);
+    }
+  }
+
+  return map;
+}
+
+function groupAnagrams2(strs) {
   if (!Array.isArray(strs) || !strs.length) return [];
   let map = new Map();
 
@@ -12,7 +29,7 @@ export default function groupAnagrams(strs) {
     if (map.has(sorted)) {
       map.get(sorted).push(char);
     } else {
-      map.set(sorted, [sorted]);
+      map.set(sorted, [char]);
     }
   }
   return map;
@@ -20,4 +37,4 @@ export default function groupAnagrams(strs) {
 
 let group = ["eat", "ate", "tea", "elbow", "lowbe"];
 console.log(groupAnagrams(group));
-console.log(groupAnagrams());
+console.log(groupAnagrams2(group));
