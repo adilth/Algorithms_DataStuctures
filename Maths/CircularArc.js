6f82870e32479ad9ec27230865d203937993848a
adil <rajaadil1995@gmail.com>
2023-08-15 19:34:44 +0100

    add test for circular arc function

--- Maths/CircularArc.js
+++ Maths/CircularArc.js
@@ -1,21 +1,24 @@
 /**Description // circular arc
- * @function circularArcLength
- * @param {Integer} radius
- * @param {Integer} degrees
  * @description 1  calculate the length of a circular arc
- * @function circularArcArea
- * @description calculate the area of the sector formed by an arc
+ * @function circularArcLength
  * @param {Integer} radius
  * @param {Integer} degrees
  */
 
 import { degreeToRadian } from "./degreeToRadian.js";
 
-const circularArcLen = (radius, degree) => radius * degreeToRadian(degree);
+export const circularArcLen = (radius, degree) =>
+  radius * degreeToRadian(degree);
 
-console.log(circularArcLen(3, 45)); //= 2.356194490192345
-const circularArcArea = (radius, degree) => {
+console.log(circularArcLen(5, 270)); //= 2.356194490192345
+/**
+ * @function circularArcArea
+ * @description calculate the area of the sector formed by an arc
+ * @param {Integer} radius
+ * @param {Integer} degrees
+ */
+export const circularArcArea = (radius, degree) => {
   return Math.pow(radius, 2) * (degreeToRadian(degree) / 2);
 };
 
-console.log(circularArcArea(3, 45)); //3.5342917352885173
+console.log(circularArcArea(1, 45)); //3.5342917352885173
