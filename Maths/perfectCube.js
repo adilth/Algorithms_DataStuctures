07d5760927837af4a54203b4e100b92606a535c6
adil <rajaadil1995@gmail.com>
2023-08-15 19:36:10 +0100

    add test for perfectcube function

--- Maths/perfectCube.js
+++ Maths/perfectCube.js
@@ -1,10 +1,12 @@
-/*Description // perfect cube
+/**Description // perfect cube
 This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors
 
  the function perfectCube takes a number as an argument and returns true if the number is a perfect cube (i.e., the cube root of the number is an integer), and false otherwise.
+ @param {Number} num
+ @returns {Boolean}
 */
 
-const perfectCube = (num) =>
+export const perfectCube = (num) =>
   Number.isFinite(num) && Math.round(Math.cbrt(num)) ** 3 === num;
 
 console.log(perfectCube(125)); //true
