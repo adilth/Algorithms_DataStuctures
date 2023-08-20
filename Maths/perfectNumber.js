8cc9c6209fac45c33154aa2654a760207f1178c7
adil <rajaadil1995@gmail.com>
2023-08-20 19:44:34 +0100

    add testing

--- Maths/perfectNumber.js
+++ Maths/perfectNumber.js
@@ -6,12 +6,22 @@
  *      So, 6 is a Perfect Number
  * Other examples of Perfect Numbers: 28, 486, ...
  */
-
-const factorsExcludingNumber = (n) => {
+/**
+ *
+ * @param {number} n
+ * @returns {Number[]}
+ */
+export const factorsExcludingNumber = (n) => {
+  if (n < 0) return [];
   return [...Array(n).keys()].filter((num) => n % num === 0);
 };
 
-const perfectNumber = (n) => {
+/**
+ *
+ * @param {Number} n
+ * @returns {Boolean}
+ */
+export const perfectNumber = (n) => {
   const factorSum = factorsExcludingNumber(n).reduce((num, initialVal) => {
     return num + initialVal;
   }, 0);
@@ -19,6 +29,6 @@ const perfectNumber = (n) => {
   return factorSum === n;
 };
 
-console.log(perfectNumber(6)); //true
-console.log(perfectNumber(28)); //true
-console.log(perfectNumber(30)); //false
+console.log(factorsExcludingNumber(0)); //true
+console.log(perfectNumber(0)); //true
+console.log(perfectNumber(-30)); //false
