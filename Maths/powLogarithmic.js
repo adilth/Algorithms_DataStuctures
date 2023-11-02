47a87cf9a3c5b797fb21868eb7e14639a58a8982
adil <rajaadil1995@gmail.com>
2023-11-02 23:23:15 +0100

    implement pow logarithmic

--- Maths/powLogarithmic.js
+++ Maths/powLogarithmic.js
@@ -7,11 +7,15 @@
 
 export default function powLog(x, n) {
   if (n == 0) return 1;
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
   const result = powLog(x, Math.floor(n / 2));
   if (n % 2 == 0) return result * result;
 
   return result * result * x;
 }
 
-console.log(powLog(7, 4)); //2401
+console.log(powLog(7, -4)); //2401
 console.log(powLog(6, 2)); //36
