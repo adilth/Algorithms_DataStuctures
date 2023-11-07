943ec689ee12cada65c659dc0542b8c64eb7622b
adil <rajaadil1995@gmail.com>
2023-11-07 23:26:38 +0100

    make jump search function accessible for testing

--- Search/jumpSearch.js
+++ Search/jumpSearch.js
@@ -10,29 +10,40 @@ Here's how the algorithm works:
 Jump search has a time complexity of O(√n) and a space complexity of O(1), which makes it an efficient algorithm for searching large arrays. 
 
 */
-
-function jumpSearch(arr, target, n = arr?.length) {
+/**
+ *
+ * @param {number} arr
+ * @param {number} target
+ * @param {number} n
+ * @returns
+ */
+export function jumpSearch(arr, target, n = arr?.length) {
   let step = Math.floor(Math.sqrt(n));
   let prev = 0;
   let curr = step;
   // Jump through the array until the target value is found or passed
 
-  while (curr < n && arr[curr] < target) {
+  while (arr[Math.min(curr, n) - 1] < target) {
     prev = curr;
     curr += step;
+    if (prev >= n) return -1;
   }
+
   // Linearly search the block starting from the previous position
-  while (prev < Math.min(curr, n)) {
-    if (arr[prev] == target) return prev;
+  while (arr[prev] < target) {
     prev++;
+
+    // If we reached next block or end of
+    // array, element is not present.
+    if (prev == Math.min(curr, n)) return -1;
   }
-  // If the target value was not found, return -1
-  return -1;
+  // If element is found
+  if (arr[prev] == target) return prev;
+  else return -1;
 }
 
-const arr = [1, 3, 5, 7, 9, 11, 13, 15];
-const val1 = 7;
-const val2 = 8;
-
-console.log(jumpSearch(arr, val1)); // Output: 3
-console.log(jumpSearch(arr, val2)); // Output: -1
+const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+const val1 = 11;
+const largeArr = Array.from({ length: 10000 }, (_, i) => i);
+console.log(jumpSearch(arr, 19)); // Output: 3
+console.log(jumpSearch(largeArr, 100)); // Output: -1
