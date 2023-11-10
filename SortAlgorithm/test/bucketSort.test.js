cedf0583f05b15d3cce1371edb38493d3b6bbde6
adil <rajaadil1995@gmail.com>
2023-11-10 23:35:24 +0100

    add testing for cocktail sort

---
+++ SortAlgorithm/test/bucketSort.test.js
@@ -0,0 +1,43 @@
+import { bucketSort } from "../bucketSort";
+
+describe("bucketSort", () => {
+  it("should sort an array of numbers using bucketSort", () => {
+    const arr = [5, 3, 1, 4, 2];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should handle an empty array", () => {
+    const arr = [];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([]);
+  });
+
+  it("should handle an array with a single element", () => {
+    const arr = [42];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([42]);
+  });
+
+  it("should handle an array that is already sorted", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should sort an array of strings in alphabetical order", () => {
+    const arr = ["banana", "apple", "cherry", "date"];
+    expect(() => bucketSort(arr)).toThrow();
+  });
+  it("should handle arrays with negative numbers", () => {
+    const arr = [-5, -2, -8, -1, -3];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([-8, -5, -3, -2, -1]);
+  });
+
+  it("should handle arrays with decimals", () => {
+    const arr = [3.14, 2.71, 1.618, 0.707];
+    const sorted = bucketSort(arr);
+    expect(sorted).toEqual([0.707, 1.618, 2.71, 3.14]);
+  });
+});
