49fac393b96ba9e86eca3bd91e3bbf37dc335edc
adil <rajaadil1995@gmail.com>
2023-11-30 23:58:55 +0100

    add test for juggler sequence function

---
+++ Maths/test/ugglerSequence.test.js
@@ -0,0 +1,30 @@
+import jugglerSequence from "../ugglerSequence";
+
+describe("jugglerSequence", () => {
+  it("should return the correct sequence for a given number", () => {
+    expect(jugglerSequence(5)).toEqual([5, 11, 36, 6, 2, 1]);
+    expect(jugglerSequence(10)).toEqual([10, 3, 5, 11, 36, 6, 2, 1]);
+    expect(jugglerSequence(1)).toEqual([1]);
+    expect(jugglerSequence(2)).toEqual([2, 1]);
+  });
+  it("should return type error for provide a negative number", () => {
+    expect(jugglerSequence(-10)).toBeInstanceOf(TypeError);
+    expect(jugglerSequence(-10).message).toEqual(
+      "please provide a positive number"
+    );
+    expect(jugglerSequence(-2).message).toEqual(
+      "please provide a positive number"
+    );
+    expect(jugglerSequence(-1).message).toBe(
+      "please provide a positive number"
+    );
+  });
+
+  it("should handle non-numeric input", () => {
+    expect(() => jugglerSequence("abc")).toThrowError("please provide number");
+    expect(() => jugglerSequence(null)).toThrowError("please provide number");
+    expect(() => jugglerSequence(undefined)).toThrowError(
+      "please provide number"
+    );
+  });
+});
